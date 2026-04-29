60c7e9976530e7ec2ea2ddf2298d8c69c46f75f5
unknown <mikhailenkoart1112@gmail.com>
2026-04-29 20:13:35 +0300

    fix gallery and mobile layout

--- src/data.js
+++ src/data.js
@@ -7,11 +7,22 @@ export const defaultData = {
       descEn: 'Warm and cozy',
       image: '/images/blanket.jpg',
     },
+    {
+      titleUa: 'Іграшки',
+      titleEn: 'Toys',
+      descUa: 'М’які handmade',
+      descEn: 'Soft handmade',
+      image: '/images/toy.jpg',
+    },
   ],
 
   gallery: [
     '/images/g1.jpg',
     '/images/g2.jpg',
+    '/images/g3.jpg',
+    '/images/g4.jpg',
+    '/images/g5.jpg',
+    '/images/g6.jpg',
     '/images/g7.jpg',
   ],
 }
