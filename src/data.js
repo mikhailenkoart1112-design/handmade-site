20cd5e58ba72ddbfeb805d859a602b3732ea4166
unknown <mikhailenkoart1112@gmail.com>
2026-04-29 19:47:07 +0300

    update site data

--- src/data.js
+++ src/data.js
@@ -7,28 +7,11 @@ export const defaultData = {
       descEn: 'Warm and cozy',
       image: '/images/blanket.jpg',
     },
-    {
-      titleUa: 'Іграшки',
-      titleEn: 'Toys',
-      descUa: 'М’які handmade',
-      descEn: 'Soft handmade',
-      image: '/images/toy.jpg',
-    },
-    {
-      titleUa: 'Інші вироби',
-      titleEn: 'Other',
-      descUa: 'Під замовлення',
-      descEn: 'Custom made',
-      image: '/images/other.jpg',
-    },
   ],
 
   gallery: [
     '/images/g1.jpg',
     '/images/g2.jpg',
-    '/images/g3.jpg',
-    '/images/g4.jpg',
-    '/images/g5.jpg',
-    '/images/g6.jpg',
+    '/images/g7.jpg',
   ],
 }
